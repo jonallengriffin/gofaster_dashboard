8cb33118e3aa53db1ff7acc9cd95d426a1f11698
William Lachance <wlach@mozilla.com>
2011-08-19 17:22:02 -0400

    Fix add-job script to actually work

--- src/dashboard/server/itbf/add-job.py
+++ src/dashboard/server/itbf/add-job.py
@@ -2,11 +2,12 @@
 
 # Stupid test script to manually add an "is this build faster" job
 
+import os
 import sys
 import queue
 
-if len(sys.argv) < 4:
-    print "Usage: %prog <tree> <revision> <submitter>"
+if len(sys.argv) < 5:
+    print "Usage: %s <tree> <revision> <submitter> <return email>" % os.path.basename(sys.argv[0])
     exit(1)
 
-queue.append_job(sys.argv[1], sys.argv[2], sys.argv[3])
+queue.append_job(sys.argv[1], sys.argv[2], sys.argv[3], sys.argv[4])
