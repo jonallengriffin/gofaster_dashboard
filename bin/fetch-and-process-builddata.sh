2698fd7512c30dbebcd521608ab31454f2194288
William Lachance <wlach@mozilla.com>
2011-09-19 18:17:27 -0400

    Add option for "local-only" mode for fetching/processing builddata
    Handy when debugging the same data set repeatedly

--- bin/fetch-and-process-builddata.sh
+++ bin/fetch-and-process-builddata.sh
@@ -8,6 +8,16 @@ SCRIPT_DIR=$(dirname $0)/../src/dashboard/server/scripts/
 DATA_DIR=$(dirname $0)/../src/dashboard/server/data/
 CSV_OUTPUT=$DATA_DIR/buildfaster.csv
 PKL_OUTPUT=$DATA_DIR/buildfaster.pkl
+LOCAL=0
 
-wget http://build.mozilla.org/builds/buildfaster.csv.gz -O - | gunzip > $CSV_OUTPUT
+while getopts l OPTION; do
+    case $OPTION in
+        l) LOCAL=1 ;;
+       \?) echo "Usage: $0 [ -l ]" && exit 2;;
+    esac
+done
+
+if [ $LOCAL == 0 ]; then
+    wget http://build.mozilla.org/builds/buildfaster.csv.gz -O - | gunzip > $CSV_OUTPUT
+fi
 python $SCRIPT_DIR/parsecsv.py $CSV_OUTPUT $PKL_OUTPUT
