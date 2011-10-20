c89a677f9884f1f2b4b7a538ecc9bfed12bcaf15
William Lachance <wlachance@mozilla.com>
2011-10-20 12:36:43 -0400

    Make runserver.sh chatter make a bit more sense

--- bin/runserver.sh
+++ bin/runserver.sh
@@ -5,9 +5,9 @@ set -e
 SERVER_DIR=$(dirname $0)/../src/dashboard/server/
 cd $SERVER_DIR && ../../../bin/python server.py &
 PID=$!
-echo "Server running with $PID"
+echo "Server running with PID $PID"
 
-trap "echo 'Killing server... $PID'; kill -9 $PID" INT TERM EXIT
+trap "echo 'Killing server...'; kill -9 $PID" INT TERM EXIT
 while true; do
     sleep 60
 done
